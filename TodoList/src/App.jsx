6ed604a0a2da2d5c991f6f784bcb35ce374972ac
rafael07-dev <deralato817@gmail.com>
2024-08-24 15:39:11 -0500

    add functionality add task

--- TodoList/src/App.jsx
+++ TodoList/src/App.jsx
@@ -1,23 +1,5 @@
-import { useState } from 'react'
+import { TodoList } from '../components/TodoList'
 import './App.css'
-
-const TodoList = () => {
-
-  const [tasks, setTasks] = useState([{ id: null, name: '', decription: '', state: null }])
-
-  return (
-    <div>
-      {tasks.map((item, index) => (
-      <div key={index} className='card-todo'>
-        <h3>{item.name}</h3>
-        <p>{item.decription}</p>
-        <p>{item.state ? 'Pendiente' : 'Finalizada'}</p>
-      </div>
-      ))}
-    </div>
-  )
-
-}
 
 function App() {
 
@@ -25,7 +7,7 @@ function App() {
     <>
       <h1>TodoList</h1>
       <section className='container'>
-        <TodoList/>
+        <TodoList />
       </section>
     </>
   )
