0367402ac6190936e4d0cef070d194c4835f3e72
rafael07-dev <deralato817@gmail.com>
2024-08-25 14:49:41 -0500

    add functionality to delete tasks

--- TodoList/components/TodoList.jsx
+++ TodoList/components/TodoList.jsx
@@ -10,22 +10,35 @@ const initialState = []
 function reducer(state, action) {
   if (action.type == ACTIONS.add_task) {
     return [
-      ...state,{
-        id: state.length +1,
+      ...state, {
+        id: state.length + 1,
         name: action.name,
         description: action.description,
         done: false
       }
     ]
   }
+
+if (action.type == ACTIONS.delete_task) {
+  const {id} = action.payload
   
+  //verificar existencia de la task por id
+  const existingTask = state.some((id) => id === id)
+
+  if (existingTask) {
+    return state.filter((item) => item.id != id);
+  }
+  
+  return state
+}
+
 }
 
 export const TodoList = () => {
 
   const [state, dispatch] = useReducer(reducer, initialState)
-  const [inputs, setInputs] = useState({inputName: '', inputDescription: ''})
-  
+  const [inputs, setInputs] = useState({ inputName: '', inputDescription: '' })
+
   function handleChange(event) {
     const { name, value } = event.target
 
@@ -35,12 +48,16 @@ export const TodoList = () => {
     }))
 
   }
-  
+
   function handleSubmit(event) {
     event.preventDefault()
-    
-    dispatch({type: 'add_task', name: inputs.inputName, description: inputs.inputDescription})
-    console.log(event);
+
+    dispatch({ type: 'add_task', name: inputs.inputName, description: inputs.inputDescription })
+    setInputs({ inputName: '', inputDescription: '' })
+  }
+
+  function deleteTask(task) {
+    dispatch({type: 'delete_task', payload: task})
   }
 
   return (
@@ -50,24 +67,21 @@ export const TodoList = () => {
         <form onSubmit={handleSubmit}>
           <input type="text" name='inputName' onChange={handleChange} placeholder='Name' value={inputs.inputName} />
           <input type="text" name='inputDescription' onChange={handleChange} placeholder='Description' value={inputs.inputDescription} />
-          <select name="state" id="state" onChange={handleChange} value={state.state}>
-            <option>--seleccionar--</option>
-            <option>Pendiente</option>
-            <option >Finalizada</option>
-          </select>
           <button type='submit'>Agregar</button>
         </form>
       </div>
-
-      {state.map((item, index) => (
-        <div key={index} className='card-todo'>
-          <h3>{item.name}</h3>
-          <p>{item.description}</p>
-          <p>{item.state ? 'Pendiente' : 'Finalizada'}</p>
-        </div>
-      ))
-      }
-
+      <div className="tasks">
+        {state.map((item, index) => (
+          <div key={index} className='card-todo'>
+            <h3>{item.name}</h3>
+            <p>{item.description}</p>
+            <p>{item.done ? 'Finalizada' : 'Pendiente'}</p>
+            <button>Editar</button>
+            <button onClick={() => deleteTask(item)}>Eliminar</button>
+          </div>
+        ))
+        }
+      </div>
     </div>
   )
 
