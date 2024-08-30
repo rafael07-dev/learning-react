65bb00b983d8ee6034a30b087678b8dc4fc59815
rafael07-dev <deralato817@gmail.com>
2024-08-30 09:19:58 -0500

    add new styles the component

--- TodoList/components/TodoList.jsx
+++ TodoList/components/TodoList.jsx
@@ -1,24 +1,25 @@
 import { useTask } from "../hooks/useTask"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash, faPenToSquare } from '@fortawesome/free-solid-svg-icons';
+import '../components/TodoList.css'
 
 export const TodoList = () => {
 
   const {state, deleteTask, editTask} = useTask()
 
   return (
-    <div>
-      <h3>Aqui está tu lista de pendientes</h3>
-      <div className="tasks">
+    <div className="tasks">
+      <h2>Todas mis tareas</h2>
         {state.map((item, index) => (
           <div key={index} className='card-todo'>
             <h3>{item.name}</h3>
             <p>{item.description}</p>
             <p>{item.done ? 'Finalizada' : 'Pendiente'}</p>
-            <button onClick={()=> editTask(item)}>Editar</button>
-            <button onClick={()=> deleteTask(item)}>Eliminar</button>
+            <a className="button" onClick={()=> editTask(item)}><FontAwesomeIcon icon={faPenToSquare} /></a>
+            <a className="button" onClick={()=> deleteTask(item)}><FontAwesomeIcon icon={faTrash} /></a>
           </div>
         ))
         }
-      </div>
     </div>
   )
 }
