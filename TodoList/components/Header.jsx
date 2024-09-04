7630e90e09f6df7c6e548bf8f256c5b60be2a0aa
rafael07-dev <deralato817@gmail.com>
2024-09-04 11:14:40 -0500

    getting value to input

--- TodoList/components/Header.jsx
+++ TodoList/components/Header.jsx
@@ -1,11 +1,16 @@
 import '../components/Header.css'
 
 export function Header(){
+
+    function handleChangeInput(e) {
+        console.log(e.target.value);
+    }
+
     return(
         <header className="header">
             <h1>Tasks To Do!</h1>
             <form action="#">
-                <input className='search-bar' type="text"  placeholder='Buscar tareas'/>
+                <input onChange={handleChangeInput} className='search-bar' type="text"  placeholder='Buscar tareas'/>
             </form>
 
             <button className='button'>Cerrar Session</button>
