8570c91181cfb4c6ff5b8fc3d5e6f39426e113b7
rafael07-dev <deralato817@gmail.com>
2024-09-07 09:36:03 -0500

    get search term input to filter tasks

--- TodoList/components/Header.jsx
+++ TodoList/components/Header.jsx
@@ -1,16 +1,35 @@
+import { useTask } from '../hooks/useTask';
+import { useEffect, useState } from 'react';
 import '../components/Header.css'
 
 export function Header(){
+    const [searchTerm, setSearchTerm] = useState('')
 
-    function handleChangeInput(e) {
-        console.log(e.target.value);
+    const {state, addToFilteredTask, setFilter} = useTask()
+    const tasks = state.task.map(item => item)    
+    
+    useEffect(()=> {
+        const filteredTasks = tasks.filter(item => item.name.includes(searchTerm) || item.description.includes(searchTerm))
+        
+        addToFilteredTask(filteredTasks)
+    }, [searchTerm])
+    
+    
+    function useHandleChangeInput(e) {
+        setSearchTerm(e.target.value)
+        setFilter('')
+    }
+
+    function handleSubmit(e){
+        console.log(e);
+        
     }
 
     return(
         <header className="header">
             <h1>Tasks To Do!</h1>
-            <form action="#">
-                <input onChange={handleChangeInput} className='search-bar' type="text"  placeholder='Buscar tareas'/>
+            <form onSubmit={handleSubmit}>
+                <input onChange={useHandleChangeInput} className='search-bar' type="text"  placeholder='Buscar tareas'/>
             </form>
 
             <button className='button'>Cerrar Session</button>
